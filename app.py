cd76793a86a0e874d595f94a7c33ee1d6f44b60e
Lucas Galana <bastosgalanalucas@gmail.com>
2026-04-09 22:33:18 -0300

    Feat: atualização da logica do app.py e adição do template edit

--- app.py
+++ app.py
@@ -42,5 +42,34 @@ def agendar():
 
     return redirect(url_for('index'))
 
+
+@app.route('/editar/<int:id>', methods=['GET', 'POST'])
+def editar(id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    if request.method == 'POST':
+        novo_cliente = request.form['cliente']
+        novo_servico = request.form['servico']
+        novo_horario = request.form['horario']
+
+        cur.execute("""
+            UPDATE agendamentos 
+            SET cliente = %s, servico = %s, horario = %s 
+            WHERE id = %s
+        """, (novo_cliente, novo_servico, novo_horario, id))
+        
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect(url_for('index'))
+
+    cur.execute("SELECT id, cliente, servico, horario FROM agendamentos WHERE id = %s", (id,))
+    agendamento = cur.fetchone()
+    cur.close()
+    conn.close()
+
+    return render_template('editar.html', agendamento=agendamento)
+
 if __name__ == '__main__':
     app.run(debug=True)
